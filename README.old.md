be9bda111c6ddad0650e1b685ed9b0ba929b3044
Arunansh Srivastava <arunansh98@gmail.com>
2025-02-06 22:57:40 +0530

    Initial Project Setup

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# billtrackr
+A SaaS subscription manager that tracks monthly payments (Netflix, Spotify, AWS, etc.), sends reminders, and calculates spending.
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+const { createRoot } = require("react-dom/client");
+const { default: App } = require("./App");
+
+const el = document.getElementById("root");
+
+const root = createRoot(el);
+
+root.render(<App />);
